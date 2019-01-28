78912dbda67122c505ea4be794fc304c060fbcff
Roman Verner <romverj@gmail.com>
2019-01-28 18:08:36 -0500

    Added movie search

--- liri.js
+++ liri.js
@@ -41,24 +41,52 @@ switch(arg) {
     // Uses Node-Spotify-API to acquire Artist + Song information
     case 'spotify-this-song':
         
-        userEntry = userEntry.join("%20").trim();
+        if (userEntry.length != 0) {
+            userEntry = userEntry.join("-").trim();
+        } else {
+            userEntry = "The-Sign";
+        };
 
         spotify.search({ type: 'track', query: userEntry }, function(err, data) {
             if (err) {
-              return console.log('Error occurred: ' + err);
+            return console.log('Error occurred: ' + err);
             };
-           
+        
             console.log(
                 "\nArtist: " + data.tracks.items[0].artists[0].name +
                 "\nSong: " + data.tracks.items[0].name +
                 "\nPreview: " + data.tracks.items[0].preview_url +
-                "\nAlbum: " + data.tracks.items[0].album.name
-            )
-        });    
+                "\nAlbum: " + data.tracks.items[0].album.name + "\n"
+            );
+        }); 
+         
         break;
 
     case 'movie-this':
+
+        if (userEntry.length != 0) {
+            userEntry = userEntry.join("+").trim();
+        } else {
+            userEntry = "Mr.+Nobody";
+        };
+
+        axios.get('http://www.omdbapi.com/?apikey=9f6a6d94&t=' + userEntry)
+            .then(function (response) {
+                console.log(
+                    "\n Title: " + response.data.Title +
+                    "\n Released: " + response.data.Year +
+                    "\n IMDB Rating: " + response.data.imdbRating +
+                    "\n Rotten Tomatoes: " + response.data.Ratings[1].Value +
+                    "\n Country: " + response.data.Country +
+                    "\n Plot: " + response.data.Plot +
+                    "\n Actors/Actresses: " + response.data.Actors + "\n"
+                );
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
         break;
+
     case 'do-what-it-says':
         break;
 }
